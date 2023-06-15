eff74eb89c167f8b4b87cdc16354d3bc69ca72fb
Nic Aplin <nic.f.aplin@gmail.com>
2023-06-15 16:36:48 +0100

    Add project files.

---
+++ PolyDice2/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using PolyDice2.Models;
+
+namespace PolyDice2;
+
+public partial class MainPage : ContentPage
+{
+	private Die Die;
+
+	public MainPage()
+	{
+		Die = new Die(20);
+        InitializeComponent();
+    }
+
+	private void OnRollClicked(object sender, EventArgs e)
+	{
+		ToggleRolling(true);
+		var rollTimer = Application.Current.Dispatcher.CreateTimer();
+		rollTimer.Interval = TimeSpan.FromSeconds(1);
+		rollTimer.Tick += (s, e) => FinishRoll(s);
+		rollTimer.Start();
+	}
+
+    private void OnLeftClicked(object sender, EventArgs e)
+    {
+		Die.Previous();
+		UpdateUI();
+    }
+
+    private void OnRightClicked(object sender, EventArgs e)
+    {
+		Die.Next();
+        UpdateUI();
+    }
+
+	private void UpdateUI()
+	{
+        OutputLbl.Text = Die.Name;
+		IconImg.Source = Die.Icon;
+
+		if (Die.Sides == 2)
+		{
+			RollBtn.Text = "Flip";
+		}
+		else
+		{
+			RollBtn.Text = "Roll";
+		}
+	}
+
+	private void FinishRoll(object sender)
+	{
+		MainThread.BeginInvokeOnMainThread(() =>
+		{
+
+			var timer = sender as IDispatcherTimer;
+			timer.Stop();
+			OutputLbl.Text = Die.Roll();
+			ToggleRolling(false);
+		});
+	}
+
+	private void ToggleRolling(bool isRolling)
+	{
+        RollBtn.IsEnabled = !isRolling;
+        OutputLbl.IsVisible = !isRolling;
+        RollAct.IsVisible = isRolling;
+        RollAct.IsRunning = isRolling;
+    }
+}
+
---
+++ PolyDice2/Models/Die.cs
@@ -0,0 +1,103 @@
+﻿namespace PolyDice2.Models
+{
+    internal class Die
+    {
+        public Die(int sides)
+        {
+            Sides = sides;
+        }
+
+        public int Sides { get; set; }
+        public string Name {
+            get
+            {
+                if (Sides == 2)
+                {
+                    return "Coin";
+                }
+
+                return $"D{Sides}";
+            }
+        }
+        public string Icon { get { return $"d{Sides}.png"; } }
+
+        public string Roll()
+        {
+            Random rng = new Random();
+            var result = rng.Next(1, Sides + 1);
+            if (Sides == 2)
+            {
+                return result == 1 ? "Heads" : "Tails";
+            }
+            return result.ToString();
+        }
+
+        public void Next()
+        {
+            switch(Sides)
+            {
+                case 2:
+                    Sides = 4;
+                    break;
+                case 4:
+                    Sides = 6;
+                    break;
+                case 6:
+                    Sides = 8;
+                    break;
+                case 8:
+                    Sides = 10;
+                    break;
+                case 10:
+                    Sides = 12;
+                    break;
+                case 12:
+                    Sides = 20;
+                    break;
+                case 20:
+                    Sides = 100;
+                    break;
+                case 100:
+                    Sides = 2;
+                    break;
+                default:
+                    Sides = 20;
+                    break;
+            }
+        }
+
+        public void Previous()
+        {
+            switch (Sides)
+            {
+                case 2:
+                    Sides = 100;
+                    break;
+                case 4:
+                    Sides = 2;
+                    break;
+                case 6:
+                    Sides = 4;
+                    break;
+                case 8:
+                    Sides = 6;
+                    break;
+                case 10:
+                    Sides = 8;
+                    break;
+                case 12:
+                    Sides = 10;
+                    break;
+                case 20:
+                    Sides = 12;
+                    break;
+                case 100:
+                    Sides = 20;
+                    break;
+                default:
+                    Sides = 20;
+                    break;
+            }
+        }
+    }
+}
